3dc09300865fb91f113a92935e48da89381c4db6
Shayne Holmes <simon.snth@xoxy.net>
2015-10-19 11:55:54 -0700

    Moving a bunch of bang logic to script

--- Skins/TimerRework/@resources/timer.lua
+++ Skins/TimerRework/@resources/timer.lua
@@ -6,10 +6,15 @@ function Initialize()
   ActiveTimerCount=tonumber(SKIN:GetVariable('ActiveTimerCount', '0'))
   TimeMeasure=SKIN:GetMeasure('MeasureTime')
   TimeOffset=0 -- represents the offset between os.clock() and Rainmeter time
+  AlarmAtEnd=0 -- if turned on, alarm plays at end
+  EndOfTimerHash=1
 end
 
 function Update()
   TimerEndOfTimer=tonumber(SKIN:GetVariable('TimerEndOfTimer', '0'))
+  if TimerEndOfTimer == nil then 
+    TimerEndOfTimer = 0
+  end
   if TimeOffset == 0 then 
     TimeOffset=TimeMeasure:GetValue() - os.clock()
   end
@@ -22,13 +27,44 @@ function Update()
     -- timer ends now
     SKIN:Bang('!SetVariable', 'TimerEndOfTimer', 0)
     SKIN:Bang('!SetVariable', 'TimerCount', '(#TimerCount#+#ActiveTimerCount#)')
-    SKIN:Bang('!WriteKeyValue', 'Variables', 'TimerCount', '(#TimerCount#+#ActiveTimerCount#)', '#@#state.inc')
     SKIN:Bang('!SetVariable', 'ActiveTimerCount ', '0')
-    SKIN:Bang('!WriteKeyValue', 'Variables', 'ActiveTimerCount', '0', '#@#state.inc')
+    SaveVariableToState('TimerEndOfTimer')
+    SaveVariableToState('TimerCount')
+    SaveVariableToState('ActiveTimerCount')
     SKIN:Bang('!CommandMeasure', 'MeasureAhkWindowMessaging', 'SendMessage 16687 1 0')
     SKIN:Bang('!EnableMeasure', 'flashCounter')
+    if AlarmAtEnd == 1 then
+      SKIN:Bang('Play', 'Alarm.Wav')
+    end
     returnVal = -1
   end
   return returnVal
 end
 
+function StartTimerAPI(duration, color, active)
+  StartTimer(duration, color, active)
+  AlarmAtEnd = 0
+end
+
+function StartTimer(duration, color, active)
+  AlarmAtEnd = 1
+  duration = tonumber(duration or 0)
+  active = active or 0
+  SKIN:Bang('!SetVariable', 'ColorTimerArc', color)
+  SKIN:Bang('!SetVariable', 'TimerEndOfTimer', duration * 60 + TimeMeasure:GetValue())
+  EndOfTimerHash = (1+EndOfTimerHash*TimeMeasure:GetValue()*0.5*(math.sqrt(5)-1)) % 1
+  SKIN:Bang('!SetVariable', 'EndOfTimerHash', EndOfTimerHash)
+  SKIN:Bang('!SetVariable', 'ActiveTimerCount', active)
+  SKIN:Bang('!UpdateMeasure', 'MeasureTimerScript')
+  SaveVariableToState('ColorTimerArc')
+  SaveVariableToState('TimerEndOfTimer')
+  SaveVariableToState('ActiveTimerCount')
+  SKIN:Bang('!Update')
+end
+
+function SaveVariableToState(variablename, value)
+  if value == nil then
+    value = '#' .. variablename .. '#'
+  end
+  SKIN:Bang('!WriteKeyValue', 'Variables', variablename, value, '#@#state.inc')
+end
