961928f0412f728fbec0c8242785f7d13a8c7dce
Shayne Holmes <simon.snth@xoxy.net>
2015-10-19 10:53:10 -0700

    Make timer script return time left
    Getting ready to take the place of MeasureTimeLeft

--- Skins/TimerRework/@resources/timer.lua
+++ Skins/TimerRework/@resources/timer.lua
@@ -1,9 +1,25 @@
 function Initialize()
-	MyVariable = 'Hello, world!'
+  -- Get variables from external state
+  TimerEndOfTimer=tonumber(SKIN:GetVariable('TimerEndOfTimer', '0'))
+  ColorTimerArc=(SKIN:GetVariable('ColorTimerArc', '0,255,0,255'))
+  TimerCount=tonumber(SKIN:GetVariable('TimerCount', '0'))
+  ActiveTimerCount=tonumber(SKIN:GetVariable('ActiveTimerCount', '0'))
+  TimeMeasure=SKIN:GetMeasure('MeasureTime')
+  TimeOffset=0 -- represents the offset between os.clock() and Rainmeter time
 end
 
 function Update()
-	return MyVariable
+  TimerEndOfTimer=tonumber(SKIN:GetVariable('TimerEndOfTimer', '0'))
+  if TimeOffset == 0 then 
+    TimeOffset=TimeMeasure:GetValue() - os.clock()
+  end
+  CurrentTime = os.clock() + TimeOffset
+  if TimerEndOfTimer > CurrentTime then
+    returnVal = TimerEndOfTimer - CurrentTime
+  else
+    returnVal = -1
+  end
+  return returnVal
 end
 
 function ChangeVar()
