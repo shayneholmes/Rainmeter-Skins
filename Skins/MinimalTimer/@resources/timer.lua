4f21e1dee69ddc125593b11e5efd7917f5d21f07
Shayne Holmes <simon.snth@xoxy.net>
2015-10-26 11:38:52 -0700

    Only alert AHK of timer end if AHK didn't start it

--- Skins/MinimalTimer/@resources/timer.lua
+++ Skins/MinimalTimer/@resources/timer.lua
@@ -30,8 +30,8 @@ function EndTimer()
   TimerEndOfTimer = 0
   SetVariable('TimerEndOfTimer', TimerEndOfTimer)
   SetVariable('TimerCount', '(#TimerCount#+#ActiveTimerCount#)')
-  SKIN:Bang('!CommandMeasure', 'MeasureAhkWindowMessaging', 'SendMessage 16687 1 0')
   if tonumber(SKIN:GetVariable('AlarmAtEnd', '0')) == 1 then
+    SKIN:Bang('!CommandMeasure', 'MeasureAhkWindowMessaging', 'SendMessage 16687 1 0')
     SKIN:Bang('!execute [Play Alarm.Wav]') 
     Flashing = FlashTimeoutSeconds + TimeMeasure:GetValue()
     SKIN:Bang('!SetVariable', 'Flashing', Flashing)
