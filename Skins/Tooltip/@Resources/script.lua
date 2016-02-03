0dc4b4f64b2ab797641bf93176cd738523f0450d
Shayne Holmes <simon.snth@xoxy.net>
2016-02-03 15:03:15 -0800

    Use the more appropriate 'work area' screen sizes

--- Skins/Tooltip/@Resources/script.lua
+++ Skins/Tooltip/@Resources/script.lua
@@ -33,8 +33,8 @@ function AlignCircles()
   SKIN:Bang('!SetOption', 'LeftCircle', 'LineLength', CircleRadius)
   SKIN:Bang('!SetOption', 'RightCircle', 'LineLength', CircleRadius)
   
-  X = tonumber(SKIN:GetVariable('PSCREENAREAX',0)) + tonumber(SKIN:GetVariable('PSCREENAREAWIDTH',0)) - W - CircleRadius*2 - 100
-  Y = tonumber(SKIN:GetVariable('PSCREENAREAY',0)) + tonumber(SKIN:GetVariable('PSCREENAREAHEIGHT',0)) - H - 100
+  X = tonumber(SKIN:GetVariable('PWORKAREAX',0)) + tonumber(SKIN:GetVariable('PWORKAREAWIDTH',0)) - W - CircleRadius*2 - 50
+  Y = tonumber(SKIN:GetVariable('PWORKAREAY',0)) + tonumber(SKIN:GetVariable('PWORKAREAHEIGHT',0)) - H - 50
   
   SKIN:Bang('!Redraw') -- update boundaries
   SKIN:Bang('!Move', X, Y) -- move skin
