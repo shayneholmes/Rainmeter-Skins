cb1008e84eb09ed8dff7f00e1e2a8031b9ff1a64
Shayne Holmes <simon.snth@xoxy.net>
2016-02-12 11:31:05 -0800

    Now smooth AND the right time.

--- Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
+++ Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
@@ -1,7 +1,11 @@
 function Initialize()
+  MeasureTime = SKIN:GetMeasure('MeasureTime')
 end
 
 function Update()
-  ClockThisTick = os.clock()
+  if not TimeOffset then
+    TimeOffset = MeasureTime:GetValue() - os.clock()
+  end
+  local ClockThisTick = os.clock() + TimeOffset
   return ClockThisTick
 end
