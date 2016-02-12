440c185a6ebdfbfa0e2797c5e969cea445182c7d
Shayne Holmes <simon.snth@xoxy.net>
2016-02-12 12:17:34 -0800

    Take stop2go logic out of script into meter
    Now with minute hand smoothness

--- Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
+++ Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
@@ -1,9 +1,5 @@
 function Initialize()
   MeasureTime = SKIN:GetMeasure('MeasureTime')
-  -- make time run fast for first 58 seconds, then sit for two seconds
-  HowOftenToWait = 60 -- top of the minute
-  HowLongToWait = 2 -- delay each time
-  GainRatio = (HowOftenToWait + HowLongToWait) / HowOftenToWait - 1
 end
 
 function Update()
@@ -11,6 +7,5 @@ function Update()
     TimeOffset = MeasureTime:GetValue() - os.clock()
   end
   local TimeValue = os.clock() + TimeOffset
-  TimeValue = TimeValue + math.min((TimeValue % HowOftenToWait) * GainRatio,HowOftenToWait-(TimeValue % HowOftenToWait))
   return TimeValue
 end
