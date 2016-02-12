b0542d97d45d05e893110da859f9b0252f69afbb
Shayne Holmes <simon.snth@xoxy.net>
2016-02-12 14:29:59 -0800

    Add debug and alert for excessive time offset

--- Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
+++ Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
@@ -7,5 +7,12 @@ function Update()
     TimeOffset = MeasureTime:GetValue() - os.clock()
   end
   local TimeValue = os.clock() + TimeOffset
+  local OffBy = TimeValue-MeasureTime:GetValue()
+  SKIN:Bang('!SetVariable', 'DebugOffset', OffBy)
+  if math.abs(OffBy) >= 1 then
+    SKIN:Bang('!SetOption', 'MeterClockBackground', 'LineColor', '255,0,0,255')
+    SKIN:Bang('!UpdateMeter', 'MeterClockBackground')
+  end
+  
   return TimeValue
 end
