782db0d62f030364d219292d8a023894038daa4d
Shayne Holmes <simon.snth@xoxy.net>
2016-02-12 11:43:32 -0800

    Introduce delay at top of the minute

--- Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
+++ Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
@@ -1,11 +1,16 @@
 function Initialize()
   MeasureTime = SKIN:GetMeasure('MeasureTime')
+  -- make time run fast for first 58 seconds, then sit for two seconds
+  HowOftenToWait = 60 -- top of the minute
+  HowLongToWait = 2 -- delay each time
+  GainRatio = (HowOftenToWait + HowLongToWait) / HowOftenToWait - 1
 end
 
 function Update()
   if not TimeOffset then
     TimeOffset = MeasureTime:GetValue() - os.clock()
   end
-  local ClockThisTick = os.clock() + TimeOffset
-  return ClockThisTick
+  local TimeValue = os.clock() + TimeOffset
+  TimeValue = TimeValue + math.min((TimeValue % HowOftenToWait) * GainRatio,HowOftenToWait-(TimeValue % HowOftenToWait))
+  return TimeValue
 end
