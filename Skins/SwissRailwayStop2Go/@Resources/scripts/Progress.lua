9544166c7214aa65e2e8252e1dbcaa2ec38ce85d
Shayne Holmes <simon.snth@xoxy.net>
2016-05-17 10:23:44 -0700

    Clarify comments for smooth timing script

--- Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
+++ Skins/SwissRailwayStop2Go/@Resources/scripts/Progress.lua
@@ -4,8 +4,8 @@ end
 
 function Update()
 -- smooth out the system clock on the minute
-  CurrentClock = os.clock() -- millisecond resolution but may not be stable for long
-  CurrentMinute = math.floor((os.time()+1)/60) -- this source has 1-second resolution but stable; sync up at :59.5 to make minute jumps tight
+  CurrentClock = os.clock() -- os.clock() has millisecond resolution but may not be stable for long
+  CurrentMinute = math.floor((os.time()+1)/60) -- os.time() has 1-second resolution but is stable; sync up at 59 seconds past the minute to make minute jumps tight
   if (not TimeOffset or (LastMinute ~= CurrentMinute)) then
     TimeOffset = MeasureTime:GetValue() - CurrentClock
     LastMinute = CurrentMinute
