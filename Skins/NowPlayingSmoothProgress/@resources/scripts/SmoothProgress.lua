1073f993ba6a0077217382a0e6edb97c6a40a3d4
Shayne Holmes <simon.snth@xoxy.net>
2016-11-22 15:16:00 -0800

    Simplify and robustify multi-object smooth progress
    Tried to shrink it, but it ended up the same length. Still, it's more
    robust now (works with out-of-bounds positions), is more performant (no
    double-updating measures), and code is easier to read.

--- Skins/NowPlayingSmoothProgress/@resources/scripts/SmoothProgress.lua
+++ Skins/NowPlayingSmoothProgress/@resources/scripts/SmoothProgress.lua
@@ -5,10 +5,6 @@ function Initialize()
   ResetInterval   = SELF:GetNumberOption('ResetInterval', 2)
   ClockThisTick   = os.clock()
   
-  ClockThisTick = os.clock()
-  ClockLastTick = ClockThisTick
-  ResetInterval = SELF:GetNumberOption('ResetInterval', 2)
-
   ActiveColorValue = 255
   InactiveColorValue = 96
   ActiveColor = ActiveColorValue .. "," .. ActiveColorValue .. "," .. ActiveColorValue
@@ -16,81 +12,85 @@ function Initialize()
 
   NumberOfSegments = SKIN:ParseFormula(SELF:GetOption('NumberOfSegments'))
   ColorField = SELF:GetOption("ColorField", "SolidColor")
+  Debug = SELF:GetOption("Debug",0)
   ObjectsToUpdate = {}
+  local j = 1
   local opt = SELF:GetOption("SegmentGroup")
-  if opt ~= "" then -- fetch more groups
-    j = 1
-    while true do
-      if opt == "" then break end
-      table.insert(ObjectsToUpdate, opt)
-      j = j + 1
-      opt = SELF:GetOption("SegmentGroup" .. j)
-    end
+  while opt ~= "" do
+    table.insert(ObjectsToUpdate, opt)
+    j = j + 1
+    opt = SELF:GetOption("SegmentGroup" .. j)
   end
   ShadeSegmentsGradually = tonumber(SELF:GetOption("ShadeSegmentsGradually", 0))
-  LastMeterSet = 0
-
-  Debug = SELF:GetOption("Debug",0)
+  LargestMeterFilled = -1
+  SetColors(1)
+  SetColors(0)
 end
 
 function Update()
-  local Duration = MeasureDuration:GetValue()
-  local ActualPosition = MeasurePosition:GetValue()
- 
-  local PlaybackState = MeasureState:GetValue()
-  
-  if PlaybackState == 0 then -- playback stopped; we can stop here
-    return 0
+  local InterpolatedPosition = GetInterpolatedPosition()
+  if #ObjectsToUpdate > 0 then
+    SetColors(InterpolatedPosition)
   end
+  return InterpolatedPosition
+end
+
+function GetInterpolatedPosition()
+  local PlaybackState = MeasureState:GetValue()
+  if PlaybackState == 0 then return 0 end-- playback stopped; we can stop here
+
+  local Duration = MeasureDuration:GetValue()
+  if Duration == 0 then return 0 end -- playback stopped; we can stop here
 
   ClockLastTick = ClockThisTick
   ClockThisTick = os.clock()
-  
-  if LastDuration ~= Duration or math.abs(InterpolatedPosition - ActualPosition) > ResetInterval then
-    -- track change or skip
+  local ActualPosition = MeasurePosition:GetValue()
+  if LastDuration ~= Duration -- new track
+     or math.abs(InterpolatedPosition - ActualPosition) > ResetInterval -- skip, or our counting is off
+     then -- reset our guess to the measured position
     LastDuration = Duration
     InterpolatedPosition = ActualPosition
-  end 
-  
-  if Stopped == 1 or Duration == 0 then -- stopped or no track
-    returnVal = 0
-  else
-    if PlaybackState == 1 then -- playing
-      InterpolatedPosition = InterpolatedPosition + (ClockThisTick - ClockLastTick)
-    end
+  elseif PlaybackState == 1 then -- playing; increment
+    InterpolatedPosition = InterpolatedPosition + (ClockThisTick - ClockLastTick)
     if Debug ~= 0 then
       SKIN:Bang('!SetVariable', 'DebugOffset', math.floor((InterpolatedPosition - ActualPosition)*1000))
     end
-    returnVal = math.min(InterpolatedPosition / Duration, 0.999)
   end
-  if #ObjectsToUpdate > 0 then
-    SetColors(returnVal)
-  end
-  return returnVal
-  
+  return clamp(InterpolatedPosition / Duration, 0, 1)
 end
 
-function SetColors(Value) -- use InterpolatedPosition to set the right squares
-  local MeterToSet = math.floor(Value * NumberOfSegments - 0.0001)
-  local step = (MeterToSet >= LastMeterSet) and 1 or -1
-  local color = (MeterToSet >= LastMeterSet) and ActiveColor or InactiveColor
-  -- print("setting from " .. LastMeterSet .. " to " .. MeterToSet .. " to color " .. color)
-  for idx, name in pairs(ObjectsToUpdate) do
-    for i = LastMeterSet, MeterToSet, step do
-      if i >= 0 and i < NumberOfSegments then
+function SetColors(Value) -- set the right objects as bright
+  local MeterToShade, IndividualPercent = math.modf(Value * NumberOfSegments)
+  LargestMeterToFill = MeterToShade - 1 -- last completely filled shape
+  if LargestMeterFilled ~= LargestMeterToFill then
+    local color, low, high
+    if LargestMeterToFill > LargestMeterFilled then -- filling more shapes (most commonly, just one shape)
+      color = ActiveColor
+      low   = clamp(LargestMeterFilled + 1,  0, NumberOfSegments)
+      high  = clamp(LargestMeterToFill,     -1, NumberOfSegments - 1)
+    else -- unfilling shapes already filled
+      color = InactiveColor
+      low   = clamp(LargestMeterToFill + 1 + (ShadeSegmentsGradually > 0 and 1 or 0),  0, NumberOfSegments)     -- gradual shading will partially color one
+      high  = clamp(LargestMeterFilled +     (ShadeSegmentsGradually > 0 and 1 or 0), -1, NumberOfSegments - 1) -- gradual shading needs erasing
+    end
+    for idx, name in pairs(ObjectsToUpdate) do
+      for i = low, high do
         SKIN:Bang('!SetOption', name .. i, ColorField, color)
         SKIN:Bang('!UpdateMeter', name .. i)
       end
     end
+    LargestMeterFilled = LargestMeterToFill
   end
-  if ShadeSegmentsGradually > 0 then
-    IndividualPercent = Value * NumberOfSegments - MeterToSet -- just the fractional part left
-    IndividualColorValue = IndividualPercent * ActiveColorValue + (1 - IndividualPercent) * InactiveColorValue
-    IndividualColor = IndividualColorValue .. "," .. IndividualColorValue .. "," .. IndividualColorValue
+  if ShadeSegmentsGradually > 0 and 0 <= MeterToShade and MeterToShade < NumberOfSegments then
+    local IndividualColorValue = IndividualPercent * ActiveColorValue + (1 - IndividualPercent) * InactiveColorValue
+    local IndividualColor = IndividualColorValue .. "," .. IndividualColorValue .. "," .. IndividualColorValue
     for idx, name in pairs(ObjectsToUpdate) do
-      SKIN:Bang('!SetOption', name .. MeterToSet, ColorField, IndividualColor)
-      SKIN:Bang('!UpdateMeter', name .. MeterToSet)
+      SKIN:Bang('!SetOption', name .. MeterToShade, ColorField, IndividualColor)
+      SKIN:Bang('!UpdateMeter', name .. MeterToShade)
     end
   end
-  LastMeterSet = MeterToSet
+end
+
+function clamp(var, low, high)
+  return var < low and low or (var > high and high or var)
 end
