ea641243a10b0423f531fa7df3458f9e30f47446
Shayne Holmes <simon.snth@xoxy.net>
2016-11-22 10:43:25 -0800

    Make new skin: NowPlayingProgressBubble
    Is a copy of the NowPlayingRound skin from the earlier SmoothProgress
    group.

---
+++ Skins/NowPlayingProgressBubble/@resources/scripts/SmoothProgress.lua
@@ -0,0 +1,48 @@
+function Initialize()
+  mDuration = SKIN:GetMeasure('mDuration')
+  mPosition = SKIN:GetMeasure('mPosition')
+  mState = SKIN:GetMeasure('mStateButton')
+  
+  InterpolatedPosition = 0
+  LastDuration = 0
+  ClockThisTick = os.clock()
+  ClockLastTick = ClockThisTick
+  ResetInterval = SELF:GetNumberOption('ResetInterval', 2) -- if the difference (in seconds) between intepolated and actual is higher than this, it will instantly reset
+end
+
+function Update()
+  -- the pattern "and X or Y" is a ternary operator (http://lua-users.org/wiki/ExpressionsTutorial)
+  
+  local Stopped = mState:GetValue() == 0 and 1 or 0
+  
+  if Stopped == 1 or Duration == 0 then -- stopped or no track
+    return 0
+  end
+
+  local Duration = mDuration:GetValue()
+  local ActualPosition = mPosition:GetValue()
+  
+  if LastDuration ~= Duration or math.abs(InterpolatedPosition - ActualPosition) > ResetInterval then
+    -- track change or skip
+    LastDuration = Duration
+    InterpolatedPosition = ActualPosition
+  end 
+  
+  local State = mState:GetValue() == 1 and 1 or 0
+
+  if State == 1 then -- playing; recalculate
+    ClockLastTick = ClockThisTick
+    ClockThisTick = os.clock()
+    
+    if ClockLastTick ~= 0 then
+      InterpolatedPosition = InterpolatedPosition + (ClockThisTick - ClockLastTick)
+    end
+  else
+    ClockThisTick = 0
+  end
+
+  return math.min(InterpolatedPosition / Duration, 0.9999)
+end
+
+--based on http://nomnuggetnom.deviantart.com/art/Muziko-for-Rainmeter-314928622
+--by Kaelri (Kaelri@gmail.com, http://kaelri.deviantart.com/)
