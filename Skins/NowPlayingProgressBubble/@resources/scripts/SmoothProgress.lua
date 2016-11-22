bdd585b899dbffd46465c1f4802ecb9ab76a764f
Shayne Holmes <simon.snth@xoxy.net>
2016-11-22 11:55:25 -0800

    Compact smooth progress lua script

--- Skins/NowPlayingProgressBubble/@resources/scripts/SmoothProgress.lua
+++ Skins/NowPlayingProgressBubble/@resources/scripts/SmoothProgress.lua
@@ -1,48 +1,34 @@
 function Initialize()
-  mDuration = SKIN:GetMeasure('MeasureDuration')
-  mPosition = SKIN:GetMeasure('MeasurePosition')
-  mState    = SKIN:GetMeasure('MeasureStateButton')
-  
-  InterpolatedPosition = 0
-  LastDuration = 0
-  ClockThisTick = os.clock()
-  ClockLastTick = ClockThisTick
+  MeasureDuration = SKIN:GetMeasure('MeasureDuration')
+  MeasurePosition = SKIN:GetMeasure('MeasurePosition')
+  MeasureState    = SKIN:GetMeasure('MeasureStateButton')
   ResetInterval = SELF:GetNumberOption('ResetInterval', 2) -- if the difference (in seconds) between intepolated and actual is higher than this, it will instantly reset
+  ClockThisTick = os.clock()
 end
 
 function Update()
-  -- the pattern "and X or Y" is a ternary operator (http://lua-users.org/wiki/ExpressionsTutorial)
+  ClockLastTick = ClockThisTick
+  ClockThisTick = os.clock()
   
-  local Stopped = mState:GetValue() == 0 and 1 or 0
-  
-  if Stopped == 1 or Duration == 0 then -- stopped or no track
+  PlaybackState = MeasureState:GetValue()
+
+  if PlaybackState == 0 then -- playback stopped; we can stop here
     return 0
   end
 
-  local Duration = mDuration:GetValue()
-  local ActualPosition = mPosition:GetValue()
+  local Duration = MeasureDuration:GetValue()
+  local ActualPosition = MeasurePosition:GetValue()
   
-  if LastDuration ~= Duration or math.abs(InterpolatedPosition - ActualPosition) > ResetInterval then
-    -- track change or skip
+  if LastDuration ~= Duration -- track change
+     or math.abs(InterpolatedPosition - ActualPosition) > ResetInterval -- skip, or our counting is off
+     then -- reset our guess to the measured position
     LastDuration = Duration
     InterpolatedPosition = ActualPosition
   end 
   
-  local State = mState:GetValue() == 1 and 1 or 0
-
-  if State == 1 then -- playing; recalculate
-    ClockLastTick = ClockThisTick
-    ClockThisTick = os.clock()
-    
-    if ClockLastTick ~= 0 then
-      InterpolatedPosition = InterpolatedPosition + (ClockThisTick - ClockLastTick)
-    end
-  else
-    ClockThisTick = 0
+  if PlaybackState == 1 then -- playing; increment
+    InterpolatedPosition = InterpolatedPosition + (ClockThisTick - ClockLastTick)
   end
 
   return math.min(InterpolatedPosition / Duration, 0.9999)
 end
-
---based on http://nomnuggetnom.deviantart.com/art/Muziko-for-Rainmeter-314928622
---by Kaelri (Kaelri@gmail.com, http://kaelri.deviantart.com/)
